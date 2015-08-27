dbc1dc62751ea24ed685a51e195b95e9e6d15cd9
nanotokyo <eanocreation@gmail.com>
2015-08-27 03:56:00 +0000

    Lesson7-work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'プロフィールを編集しました'
+    else
+      render 'edit'
+    end
+  end
+
+  
 
   def show
     @user = User.find(params[:id])
@@ -22,6 +36,10 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
 end
